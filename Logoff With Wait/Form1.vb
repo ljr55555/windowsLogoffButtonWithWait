caaf3082f1733fa17994211e40cdb23351e7a6b3
Lisa Rushworth <github@lisa.rushworth.us>
2019-03-08 17:16:41 -0500

    Initial code

---
+++ Logoff With Wait/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Dim iLogOffDelay As Integer = 30
+    Dim boolSlashes As Boolean = True
+    'MessageBox.Show(My.Computer.Info.OSFullName)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim osVer As Version = Environment.OSVersion.Version
+        If boolSlashes Then
+            'MessageBox.Show("shutdown /l /f /t " & iLogOffDelay)
+            Shell("shutdown /l /f /t " & iLogOffDelay)
+        Else
+            'MessageBox.Show("shutdown -l -f -t " & iLogOffDelay)
+            Shell("shutdown -l -f -t " & iLogOffDelay)
+        End If
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs)
+        'MessageBox.Show("shutdown /a")
+        Shell("shutdown -a")
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
